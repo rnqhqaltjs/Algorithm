5f0af590bb7815012e0666282bb0ffe80b8bf3c8
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-01-01 15:21:59 +0900

    [level unrated] Title: 배열 뒤집기, Time: 18.54 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -0,0 +1,8 @@
+class Solution {
+    fun solution(num_list: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        answer = num_list
+        answer.reverse()
+        return answer
+    }
+}
