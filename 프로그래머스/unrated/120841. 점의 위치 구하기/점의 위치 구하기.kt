d66109b4e8455e962def7ede568e3afeb763e8aa
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-01-05 15:52:51 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(dot: IntArray): Int {
+        var answer: Int = 0
+        if(dot[0]>0 && dot[1]>0) {
+            answer = 1
+        } else if (dot[0]>0 && dot[1]<0) {
+            answer = 4
+        } else if (dot[0]<0 && dot[1]> 0) {
+            answer = 2
+        } else {
+            answer = 3
+        }
+        
+        return answer
+    }
+}
