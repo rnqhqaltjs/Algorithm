6f77989c5545065592df299c1213da42895687d8
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-01-10 20:09:10 +0900

    [level 2] Title: 귤 고르기, Time: 225.71 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun solution(k: Int, tangerine: IntArray): Int {
-        var list = hashMapOf<Int, Int>()
-        var answer = intArrayOf()
+        var list = IntArray(tangerine.maxOrNull()!! + 1)
         var k = k
         var count = 0
-        
-        for(i in tangerine) {
-            list[i] = list.getOrDefault(i, 0) + 1
+
+        for (i in tangerine) {
+            list[i]++
         }
-        answer = list.values.sortedDescending().toIntArray()
         
-        for(i in answer) {
-            count ++
-            k-=i
-            if(k<=0) {
+        list = list.sortedDescending().toIntArray()
+
+        for (i in list) {
+            count++
+            k -= i
+            if (k <= 0) {
                 break
-            }    
+            }
         }
         return count
     }
