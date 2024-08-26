c4d35bf291a7799f6472b5ce3a9d50062e3cc496
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-08-26 14:18:12 +0900

    [Silver V] Title: 올림픽, Time: 268 ms, Memory: 30040 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.kt
+++ 백준/Silver/8979. 올림픽/올림픽.kt
@@ -2,11 +2,11 @@ val br = System.`in`.bufferedReader()
 
 fun main() = with(System.out.bufferedWriter()) {
     val (n, k) =br.readLine().split(" ").map { it.toInt() }
-
+    var idx = 0
 
     val table = Array(n) { IntArray(4) }
 
-    repeat(n) { i ->
+    for(i in 0 until n) {
         table[i] =br.readLine().split(" ").map { it.toInt() }.toIntArray()
     }
 
@@ -14,7 +14,13 @@ fun main() = with(System.out.bufferedWriter()) {
 
     for(i in 0 until n) {
         if(table[i][0] == k) {
+            idx = i
+        }
+    }
+    for(i in 0 until n) {
+        if(table[idx][1] == table[i][1] && table[idx][2] == table[i][2] && table[idx][3] == table[i][3]) {
             print(i+1)
+            break
         }
     }
     close()
