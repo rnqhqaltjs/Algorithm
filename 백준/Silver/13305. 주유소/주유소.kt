e351cab94d1bf25e3e4b2fea1bda112fcc034b6d
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-08-27 20:08:06 +0900

    [Silver III] Title: 주유소, Time: 336 ms, Memory: 39812 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.kt
@@ -0,0 +1,20 @@
+val br = System.`in`.bufferedReader()
+
+fun main() = with(System.out.bufferedWriter()) {
+    val n = br.readLine().toInt()
+
+    val distance = br.readLine().split(" ").map { it.toInt() }
+    val cost = br.readLine().split(" ").map { it.toInt() }
+    var minCost = cost[0]
+    var sum = 0
+
+    for(i in 0 until n-1) {
+        if(minCost>cost[i]) {
+            minCost = cost[i]
+        }
+        sum +=distance[i] * minCost
+    }
+
+    write("$sum")
+    close()
+}
