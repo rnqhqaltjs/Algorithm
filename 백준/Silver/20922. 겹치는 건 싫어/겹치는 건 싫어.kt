550c31fec8a8c46b2782e96c4c7fc77c83e10993
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-09-03 01:00:43 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 548 ms, Memory: 59728 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.kt
@@ -0,0 +1,25 @@
+import kotlin.math.max
+
+val br = System.`in`.bufferedReader()
+
+fun main() = with(System.out.bufferedWriter()) {
+    val (n, k) = br.readLine().split(" ").map { it.toInt() }
+    val a = br.readLine().split(" ").map { it.toInt() }
+    val map = mutableMapOf<Int, Int>()
+    var max = 0
+    var left = 0
+
+    for(i in 0 until n) {
+        map[a[i]] = map.getOrDefault(a[i],0) +1
+
+        while(map[a[i]]!!> k) {
+            map[a[left]] = map[a[left]]!! - 1
+            left++
+        }
+
+        max = max(max, i-left+1)
+    }
+
+    write("$max")
+    close()
+}
