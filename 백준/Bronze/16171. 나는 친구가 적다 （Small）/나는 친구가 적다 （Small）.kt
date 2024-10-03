b7b902c9cd1689a90f08f1ca461dba2fee120995
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-10-03 17:18:58 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 124 ms, Memory: 19776 KB -BaekjoonHub

--- 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.kt
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.kt
@@ -1,16 +1,16 @@
 val br = System.`in`.bufferedReader()
 val number = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 fun main() = with(System.out.bufferedWriter()) {
-    val s = br.readLine().toCharArray()
-    val k = br.readLine().toCharArray()
-    val temp = mutableListOf<Char>()
+    val s = br.readLine()
+    val k = br.readLine()
+    var temp = ""
     for (i in s.indices) {
         if (s[i] !in number) {
-            temp.add(s[i])
+            temp+=s[i]
         }
     }
 
-    if (String(temp.toCharArray()).contains(String(k))) {
+    if (temp.contains(k)) {
         write("1")
     } else {
         write("0")
