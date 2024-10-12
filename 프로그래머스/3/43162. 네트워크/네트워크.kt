41f4b27a1c6daa5da501ef6bfbf4bde6aa41e6ac
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-10-13 00:21:30 +0900

    [level 3] Title: 네트워크, Time: 0.58 ms, Memory: 60.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.kt
+++ 프로그래머스/3/43162. 네트워크/네트워크.kt
@@ -14,26 +14,19 @@ class Solution {
             }
         }
                 
-        fun bfs(s: Int, graph:Array<MutableList<Int>>, v:BooleanArray) {
+        fun dfs(s: Int, graph:Array<MutableList<Int>>, v:BooleanArray) {
             v[s] = true
-            val q = LinkedList<Int>()
-            q.offer(s)
             
-            while(q.isNotEmpty()) {
-                val c = q.poll()
-                
-                for(i in graph[c]) {
-                    if(!visited[i]) {
-                        q.offer(i)
-                        v[i] = true
-                    }
+            for(i in graph[s]) {
+                if(!v[i]) {
+                    dfs(i, graph, v)
                 }
             }
         }
         
         for(i in 0 until n) {
             if(!visited[i]) {
-                bfs(i, graph, visited)
+                dfs(i, graph, visited)
                 answer++
             }
         }
