68e8bc0bebbb8acc91a8c1e905e9792267bae92e
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-10-28 16:49:39 +0900

    [level 2] Title: 배달, Time: 15.65 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.kt
@@ -0,0 +1,36 @@
+import java.util.*
+
+class Solution {
+    fun solution(N: Int, road: Array<IntArray>, k: Int): Int {
+        val graph = Array(N + 1) { mutableListOf<Pair<Int, Int>>() }
+        
+        for(i in road) {
+            val (a, b, c) = i
+            graph[a].add(Pair(b, c))
+            graph[b].add(Pair(a, c))
+        }
+        
+        return dijkstra(1, graph, N, k)
+    }
+    
+    fun dijkstra(s: Int, graph: Array<MutableList<Pair<Int, Int>>>, n: Int, k: Int): Int {
+        val dist = IntArray(n + 1) { Int.MAX_VALUE }
+        dist[s] = 0
+        val pq = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+        pq.add(s to 0)
+        
+        while (pq.isNotEmpty()) {
+            val (current, currentDist) = pq.poll()
+            
+            for((next, time) in graph[current]) {
+                val newDist = currentDist + time
+                if(newDist< dist[next]) {
+                    dist[next] = newDist
+                    pq.add(next to newDist)
+                }
+            }
+        }
+        
+        return dist.count { it <= k }
+    }
+}
