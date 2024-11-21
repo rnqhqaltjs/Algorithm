db931f43d80a76d7274b9e1f140f74cec44b00dd
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-11-21 20:46:41 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 38.59 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.kt
@@ -1,7 +1,13 @@
 class Solution {
     fun solution(s: String): String {
-        var list = s.split(" ").map{it.toInt()}
-        var answer = "${list.minOrNull()} ${list.maxOrNull()}"
-        return answer
+        var max = Int.MIN_VALUE
+        var min = Int.MAX_VALUE
+        var array = s.split(" ").map{ it.toInt() }
+        
+        for(i in array) {
+            min = minOf(min, i)
+            max = maxOf(max, i)
+        }
+        return "$min $max"
     }
 }
