03a00beb8128a7c476bd19be68edbddc54fd4d2e
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-11-23 20:52:04 +0900

    [level 2] Title: 할인 행사, Time: 91.90 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.kt
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.kt
@@ -1,23 +1,22 @@
 class Solution {
     fun solution(want: Array<String>, number: IntArray, discount: Array<String>): Int {
-        var productMap: MutableMap<String,Int> = hashMapOf()
-        var discountMap: MutableMap<String,Int> = hashMapOf()
         var answer: Int = 0
-        
-        
+        val wantMap = mutableMapOf<String, Int>()
+         
         for(i in want.indices) {
-            productMap[want[i]] = number[i]
+            wantMap[want[i]] = number[i]
         }
-        for(i in 0..discount.size-10) { 
-            discountMap = hashMapOf()
-            for(j in i until i+10) {
+        
+        for(i in 0..discount.size - 10) {
+            val discountMap = mutableMapOf<String, Int>()
+            for(j in i until i + 10) {
                 discountMap[discount[j]] = discountMap.getOrDefault(discount[j], 0) + 1
             }
-            if(productMap == discountMap) {
+            
+            if(wantMap == discountMap) {
                 answer++
             }
         }
-        
         return answer
     }
 }
