4c5ed607116b2d0653d61d1a815ffe02dc7f5b99
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-11-28 03:40:26 +0900

    [level 2] Title: 타겟 넘버, Time: 16.43 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,20 +1,20 @@
+var answer = 0
+
 class Solution {
     fun solution(numbers: IntArray, target: Int): Int {
-        var answer = 0
-        
-        fun dfs(count: Int, result: Int) {
-            if(numbers.size==count) {
-                if(target==result) {
-                    answer++
-                }
-            } else {
-                dfs(count+1, result+numbers[count])
-                dfs(count+1, result-numbers[count])
-            }
-        }
-        
-        dfs(0,0)
+        dfs(0, numbers, target, 0)
         
         return answer
+    }
+    
+    fun dfs(count: Int, numbers: IntArray, target: Int, result: Int) {
+        if(count == numbers.size) {
+            if(result == target) {
+                answer++
+            }
+        } else {
+            dfs(count + 1, numbers, target, result + numbers[count])
+            dfs(count + 1, numbers, target, result - numbers[count])
+        }
     }
 }
