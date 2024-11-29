201cbf291333937b0ed9d94394b701e65c021b6e
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-11-30 02:10:44 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 34.87 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.kt
@@ -1,23 +1,13 @@
 class Solution {
     fun solution(food: IntArray): String {
-        var answer = "0"
-        
-        for(i in food.size - 1 downTo 1) {
-            if(food[i] % 2 == 0) {
-                for(j in 1..food[i]/2) {
-                    answer = i.toString() + answer
-                    answer = answer + i.toString()
-                }
-            } else {
-                for(j in 1..(food[i]-1)/2) {
-                    answer = i.toString() + answer
-                    answer = answer + i.toString()
-                }
+        var answer: String = ""
+        for(i in 1 until food.size) {
+            for (j in 1..food[i]/2) {
+                answer+=i
             }
-            
-            println(i)
-            println(answer)
         }
-        return answer
+
+        
+        return answer+ 0*food[0] + answer.reversed()
     }
 }
