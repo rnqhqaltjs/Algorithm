54d14b6435508395b87965c8d9fd0e9de295a976
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-01 22:03:19 +0900

    [level 1] Title: 옹알이 (2), Time: 12.33 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.kt
@@ -2,23 +2,21 @@ class Solution {
     fun solution(babbling: Array<String>): Int {
         var answer: Int = 0
         
-        
         for(i in babbling.indices) {
-            if(babbling[i].contains("ayaaya") ||
-               babbling[i].contains("yeye") ||
-               babbling[i].contains("woowoo") ||
-               babbling[i].contains("mama")) 
-            {
-                continue
+            if(!babbling[i].contains("ayaaya") && !babbling[i].contains("yeye") && !babbling[i].contains("woowoo") && !babbling[i].contains("mama")) {   
+                babbling[i] = babbling[i].replace("aya", " ")
+                babbling[i] = babbling[i].replace("ye", " ")
+                babbling[i] = babbling[i].replace("woo", " ")
+                babbling[i] = babbling[i].replace("ma", " ")
+                babbling[i] = babbling[i].replace(" ", "")
             }
-            babbling[i] = babbling[i].replace("aya", " ")
-            babbling[i] = babbling[i].replace("ye", " ")
-            babbling[i] = babbling[i].replace("woo", " ")
-            babbling[i] = babbling[i].replace("ma", " ")
-            babbling[i] = babbling[i].replace(" ", "")
             
-            if (babbling[i].isEmpty()) answer++
+            if(babbling[i].isEmpty()) {
+                answer++
+            }
         }
+        
+        
         return answer
     }
 }
