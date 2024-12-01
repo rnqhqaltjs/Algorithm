48cb3e24781d93d349262761d1902232874f4efd
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-01 21:31:41 +0900

    [level 1] Title: 소수 만들기, Time: 2.75 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.kt
@@ -1,21 +1,28 @@
+import kotlin.math.*
+
 class Solution {
-    fun isPrime(num: Int): Boolean {
-        if (num <= 1) return false 
-        return (2..Math.sqrt(num.toDouble()).toInt()).none { num % it == 0 }
-    }
-    
     fun solution(nums: IntArray): Int {
         var answer = 0
  
-        for(i in 0..nums.size-1) {
-            for(j in i+1..nums.size-1) {
-                for(k in j+1..nums.size-1) {
+        for(i in 0 until nums.size) {
+            for(j in i+1 until nums.size) {
+                for(k in j+1 until nums.size) {
                     if(isPrime(nums[i]+nums[j]+nums[k])) {
                         answer++
                     }
                 }
             }
         }
+
         return answer
+    }
+    
+    fun isPrime(n: Int) : Boolean {
+        val sqrt = sqrt(n.toDouble()).toInt()
+
+        for(i in 2..sqrt) {
+            if( n % i == 0 ) return false
+        }
+        return true
     }
 }
