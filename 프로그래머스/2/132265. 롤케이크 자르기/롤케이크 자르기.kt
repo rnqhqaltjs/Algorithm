0abc891b38091376cb3b6a3015b2357f6a6c99df
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-05 20:02:42 +0900

    [level 2] Title: 롤케이크 자르기, Time: 225.40 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
@@ -10,9 +10,11 @@ class Solution {
         
         for(i in topping) {
             map1[i] = map1.getOrDefault(i, 0) - 1
-            if(map1[i]== 0) {
+            
+            if(map1[i] == 0) {
                 map1.remove(i)
             }
+            
             map2[i] = map2.getOrDefault(i, 0) + 1
             
             if(map1.size == map2.size) {
