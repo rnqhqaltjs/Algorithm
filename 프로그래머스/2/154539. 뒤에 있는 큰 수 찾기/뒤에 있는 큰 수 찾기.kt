d4807e5e24c6d0eb41545bb78354c37dafe76dcd
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-06 02:14:21 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 71.74 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.kt
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.kt
@@ -2,13 +2,14 @@ import java.util.*
 
 class Solution {
     fun solution(numbers: IntArray): IntArray {
-        val answer = IntArray(numbers.size) { -1 }
+        var answer = IntArray(numbers.size) { -1 }
         val stack = Stack<Int>()
         
         for(i in numbers.indices) {
-            while(stack.isNotEmpty() && numbers[stack.peek()] < numbers[i]) {
+            while(stack.isNotEmpty() && numbers[i] > numbers[stack.peek()]) {
                 answer[stack.pop()] = numbers[i]
             }
+            
             stack.push(i)
         }
         return answer
