ba333b694789cb547818f3bc38b3a0ece350a9da
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-06 16:33:15 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 27.75 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.kt
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.kt
@@ -1,17 +1,34 @@
+import kotlin.math.*
+
 class Solution {
-    fun isPrime(num: Long): Boolean {
-        if (num <= 1) return false 
-        return (2..Math.sqrt(num.toDouble()).toLong()).none { num % it == 0L }
-    }
-    
     fun solution(n: Int, k: Int): Int {
-        var answer: Int = 0
-        var list = n.toString(k).split("0").filter{it.isNotEmpty()}.map{it.toLong()}
+        var answer = 0
+        val list = n.toString(k).split("0").filter { it.isNotEmpty() }
+
+        println(list)
+        
         for(i in list) {
-            if(isPrime(i)) {
+            if(isPrime(i.toLong())) {
                 answer++
             }
         }
         return answer
+    }
+    
+    fun isPrime(n: Long): Boolean {
+        
+        if(n==1L) {
+            return false
+        }
+        
+        val sqrt = sqrt(n.toDouble()).toLong()
+        
+        for(i in 2..sqrt) {
+            if(n%i==0L) {
+                return false
+            }
+        }
+        
+        return true
     }
 }
