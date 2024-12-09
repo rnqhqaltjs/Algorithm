f46b26688519a07e14ede8718faf81d1422bb779
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-09 20:46:42 +0900

    [level 2] Title: 숫자 변환하기, Time: 93.32 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.kt
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.kt
@@ -1,35 +1,36 @@
-import java.util.*
+import java.util.* 
 
 class Solution {
     fun solution(x: Int, y: Int, n: Int): Int {
         var answer: Int = 0
         val visited = BooleanArray(y+1)
-        return bfs(x,y,n,visited)
+        
+        return bfs(x, y, n, visited)
     }
     
-    fun bfs(s: Int, e: Int, n:Int, v: BooleanArray): Int {
-        val q = LinkedList<Pair<Int,Int>>()
+    fun bfs(s: Int, e:Int, n: Int, v: BooleanArray): Int {
+        val q = LinkedList<Pair<Int, Int>>()
         v[s] = true
         q.offer(s to 0)
         
         while(q.isNotEmpty()) {
             val (c, count) = q.poll()
             
-            if(c == e) {
+            if(c==e) {
                 return count
             }
             
-            if(c + n in v.indices && !v[c+n]) {
+            if(c+n in v.indices && !v[c+n]) {
                 v[c+n] = true
                 q.offer(c+n to count + 1)
             }
             
-            if(c*2 in v.indices && !v[c*2]) {
+            if(c * 2 in v.indices && !v[c*2]) {
                 v[c*2] = true
                 q.offer(c*2 to count + 1)
             }
             
-            if(c*3 in v.indices && !v[c*3]) {
+            if(c * 3 in v.indices && !v[c*3]) {
                 v[c*3] = true
                 q.offer(c*3 to count + 1)
             }
