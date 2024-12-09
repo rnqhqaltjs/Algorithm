a9d4309352e0033eb4d5e962e293259d39a07cf5
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-09 22:05:06 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 68.44 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.kt
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.kt
@@ -2,42 +2,40 @@ import java.util.*
 
 class Solution {
     fun solution(queue1: IntArray, queue2: IntArray): Int {
+        var answer: Int = -2
         val q1 = ArrayDeque<Long>()
         val q2 = ArrayDeque<Long>()
         
-        for(i in queue1) {
-            q1.offer(i.toLong())
-        }
-        
-        for(i in queue2) {
-            q2.offer(i.toLong())
+        for(i in queue1.indices) {
+            q1.offer(queue1[i].toLong())
+            q2.offer(queue2[i].toLong())
         }
         
         var sum1 = q1.sum()
         var sum2 = q2.sum()
         
         var idx = 0
-        var max = q1.size * 4
-        while(idx <= max) {
+        val max = q1.size * 4
+        while(idx<=max) {
             if(sum1 == sum2) {
                 return idx
             }
-            
             if(sum1>sum2) {
                 val element = q1.removeFirst()
                 q2.offer(element)
-                sum2 += element
+                
                 sum1 -= element
+                sum2 += element
             } else {
                 val element = q2.removeFirst()
                 q1.offer(element)
+                
                 sum1 += element
                 sum2 -= element
             }
             
             idx++
         }
-        
         return -1
     }
 }
