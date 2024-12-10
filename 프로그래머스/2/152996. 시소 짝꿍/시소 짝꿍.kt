65afde5228e6929adcadd8bea64c861979ec87e3
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-10 18:07:28 +0900

    [level 2] Title: 시소 짝꿍, Time: 8784.50 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.kt
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.kt
@@ -1,17 +1,17 @@
 class Solution {
     fun solution(weights: IntArray): Long {
         var answer: Long = 0
-        val weight = weights.sorted().toIntArray()
+        val w = weights.sorted().toIntArray()
         
-        for(i in weight.indices) {
-            for(j in i+1 until weight.size) {
-                if(weight[i] == weight[j]) {
+        for(i in w.indices) {
+            for(j in i+1 until w.size) {
+                if(w[i] == w[j]) {
                     answer++
-                } else if (weight[i] * 2 == weight[j]) {
+                } else if (w[i] * 2 == w[j]) {
                     answer++
-                } else if (weight[i] * 3 == weight[j] * 2) {
+                } else if (w[i] * 3 == w[j] * 2) {
                     answer++
-                } else if (weight[i] * 4 == weight[j] * 3) {
+                } else if (w[i] * 4 == w[j] * 3) {
                     answer++
                 }
             }
