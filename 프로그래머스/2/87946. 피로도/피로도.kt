fa0a228f5389349f7d772297c8d207062df607d9
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-10 22:07:17 +0900

    [level 2] Title: 피로도, Time: 3.92 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.kt
+++ 프로그래머스/2/87946. 피로도/피로도.kt
@@ -1,22 +1,22 @@
 class Solution {
+    var answer: Int = -1
+    
     fun solution(k: Int, dungeons: Array<IntArray>): Int {
-        var answer: Int = -1
         val visited = BooleanArray(dungeons.size)
-        
-        fun dfs(n: Int, count:Int) {
-            answer = maxOf(answer, count)
-            
-            for(i in dungeons.indices) {
-                if(!visited[i] && n>=dungeons[i][0]) {
-                    visited[i] = true
-                    dfs(n-dungeons[i][1], count + 1)
-                    visited[i] = false
-                }
-            }
-        }
-                
-        dfs(k, 0)
+        dfs(k, dungeons, visited, 0)
         
         return answer
+    }
+    
+    fun dfs(s: Int, dungeons: Array<IntArray>, v: BooleanArray, count: Int) {
+        answer = maxOf(answer, count)
+        
+        for(i in dungeons.indices) {
+            if(s>=dungeons[i][0] && !v[i]) {
+                v[i] = true
+                dfs(s - dungeons[i][1], dungeons, v, count + 1)
+                v[i] = false
+            }
+        }
     }
 }
