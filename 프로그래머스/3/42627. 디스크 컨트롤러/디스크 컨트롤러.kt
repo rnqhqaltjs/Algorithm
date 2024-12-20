8beecb4849712cdfefd6047ea7c79fdcc76f682e
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2024-12-20 19:30:59 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.90 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+class Solution {
+    fun solution(jobs: Array<IntArray>): Int {
+        var answer = 0
+        
+        val pq1 = PriorityQueue<Pair<Int, Int>>(compareBy { it.first })
+        val pq2 = PriorityQueue<Pair<Int, Int>>(compareBy { it.second })
+        
+        for(i in jobs) {
+            pq1.add(i[0] to i[1])
+        }
+        
+        var time = 0
+        while(pq1.isNotEmpty() || pq2.isNotEmpty()) {
+            while(pq1.isNotEmpty() && pq1.peek().first<=time) {
+                pq2.offer(pq1.poll())
+            }
+            
+            if(pq2.isNotEmpty()) {
+                val (request, during) = pq2.poll()
+                time += during
+                answer += time - request
+            } else if (pq1.isNotEmpty()) {
+                time = pq1.peek().first
+            }
+        }
+        
+        
+        return answer / jobs.size
+    }
+}
