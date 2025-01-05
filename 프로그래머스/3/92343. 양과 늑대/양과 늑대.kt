77f2778317247475ce11d9ed9ac376e2601c46be
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2025-01-06 02:28:31 +0900

    [level 3] Title: 양과 늑대, Time: 4.75 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(info: IntArray, edges: Array<IntArray>): Int {
+        var answer: Int = 0
+        val edge = Array(edges.size) { mutableListOf<Int> }
+        
+        for(i in edges) {
+            i[
+        }
+        return answer
+    }
+    
+    fun dfs() {
+        
+    }
+}
