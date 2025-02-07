7dc9550872273fa3ea57c3b62f8cf29f7468a8cf
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2025-02-07 22:54:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.kt
@@ -1,15 +1,16 @@
 class Solution {
     fun solution(n: Int, a: Int, b: Int): Int {
         var answer = 0
- 
+        
         var a = a
         var b = b
-        for(i in 1..n) {
-            a = (a+1)/2
-            b = (b+1)/2
+        
+        while(true) {
+            answer++
+            a = (a + 1) / 2
+            b = (b + 1) / 2
             
-            if(a==b) {
-                answer = i
+            if(a == b) {
                 break
             }
         }
