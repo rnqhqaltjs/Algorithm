458a10242801329e17f7caac7dcc9e40e46859ab
김승우 <86480696+rnqhqaltjs@users.noreply.github.com>
2025-02-09 22:40:58 +0900

    [level 2] Title: 롤케이크 자르기, Time: 360.00 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(topping: IntArray): Int {
-        var answer: Int = 0
+        var answer = 0
+        
         val map1 = mutableMapOf<Int, Int>()
         val map2 = mutableMapOf<Int, Int>()
         
@@ -10,11 +11,9 @@ class Solution {
         
         for(i in topping) {
             map1[i] = map1.getOrDefault(i, 0) - 1
-            
-            if(map1[i] == 0) {
+            if(map1[i]!! == 0) {
                 map1.remove(i)
             }
-            
             map2[i] = map2.getOrDefault(i, 0) + 1
             
             if(map1.size == map2.size) {
